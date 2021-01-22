974cb0d99ed402a8b120d2cf985a096663029c6f
moein ghobbeh <m.ghobbeh@vriday.net>
2021-01-22 18:02:33 +0100

    add sender email module

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from web import db, login_manager
 import datetime
 from flask_login import UserMixin
@@ -19,7 +20,24 @@ class User(db.Model, UserMixin):
                f'{self.email})'
 
 
-
 class Role(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    rolename = db.Column(db.String(20),nullable=False)
+    rolename = db.Column(db.String(20), nullable=False)
+
+
+class Event(db.Model, UserMixin):
+    event_id = db.Column(db.Integer, primary_key=True)
+    eventid = db.Column(db.String, nullable=False)
+    eventname = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.String(150))
+    startdate = db.Column(db.DateTime, nullable=False)
+    enddate = db.Column(db.DateTime, nullable=False)
+    capacity = db.Column(db.Integer)
+    ipAddress = db.Column(db.String)
+
+
+class participant(db.Model, UserMixin):
+    participant_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(50))
+    event_id = db.Column(db.String(50))  # ???????
